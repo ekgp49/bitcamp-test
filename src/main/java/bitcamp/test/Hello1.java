b5602d4181b718b2a0fbcaa3f670a7f9a8d2d327
JUNE-29 <jiyuni529@gmail.com>
2020-03-31 09:45:42 +0900

    Hello1 수정

--- src/main/java/bitcamp/test/Hello1.java
+++ src/main/java/bitcamp/test/Hello1.java
@@ -3,7 +3,14 @@ package bitcamp.test;
 public class Hello1 {
 
   public static void main(String[] args) {
-    System.out.println("김다혜");
+    System.out.println("김다혜 짱짱맨~~~~~~~~");
+
+    int zzang = 1000;
+    if (zzang < 100) {
+      System.out.println("짱짱맨");
+    } else {
+      System.out.println("아 더 짱짱맨");
+    }
   }
 
 }
