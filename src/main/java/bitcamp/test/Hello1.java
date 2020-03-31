24aca1fb4639bc6f49bc214cb4816681dd761eb9
DongYoungSon <ehddud2978@gmail.com>
2020-03-31 09:45:21 +0900

    update

--- src/main/java/bitcamp/test/Hello1.java
+++ src/main/java/bitcamp/test/Hello1.java
@@ -4,6 +4,11 @@ public class Hello1 {
 
   public static void main(String[] args) {
     System.out.println("김다혜");
+    System.out.println("손동영 추가!");
+
+    // 공부말고 한강가서 놀고 싶어요!!!!!!
   }
+
+
 
 }
