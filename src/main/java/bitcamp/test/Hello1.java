e1b1297fafcad148706e334c187cb27fbbc311d3
eunjeong <euuun22@naver.com>
2020-03-31 16:46:28 +0900

    수정

--- src/main/java/bitcamp/test/Hello1.java
+++ src/main/java/bitcamp/test/Hello1.java
@@ -3,27 +3,7 @@ package bitcamp.test;
 public class Hello1 {
 
   public static void main(String[] args) {
-    System.out.println("김다혜 짱짱맨~~~~~~~~");
-    System.out.println("김다혜");
-    System.out.println("은정!!!!!!!");//
-    System.out.println("수정되나");
-
-    int zzang = 1000;
-    if (zzang < 100) {
-      System.out.println("짱짱맨");
-    } else {
-      System.out.println("아 더 짱짱맨");
-    }
-    System.out.println("노기현.");
-    System.out.println("..");
-    System.out.println("핫!!");
-
-    System.out.println("동영변경");
-    System.out.println("날씨좋당!!");
-    System.out.println("변경2");
-
-    System.out.println("변경1");
-
+    System.out.println("내가 다한다!!!!");
   }
 
 }
