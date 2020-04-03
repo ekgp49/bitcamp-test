4fc2b1db0c6bdceb8b126f8cf5ffa66b441a8be6
DongYoungSon <ehddud2978@gmail.com>
2020-04-03 14:18:07 +0900

    (수정)program_coaching_systems 제외

--- project_ERD/dateExample_All.sql
+++ project_ERD/dateExample_All.sql
@@ -1,11 +1,6 @@
 
 /*
- * calendar_files
-member_coaching_programs
-
-workout_tags
-coaching_program_tags
-coaching_systems
+ * 
 program_coaching_systems
 
 *입력 안되는 테이블*/
@@ -192,6 +187,50 @@ insert into to_do_list(to_do_list_no, member_no, memo, create_date)
 insert into to_do_list(to_do_list_no, member_no, memo, create_date)
   values(10, 7, '일주일에 3일이상 운동하기', '2020-03-04');
 
+  
+  
+  -- member_coaching_programs 예제 데이터
+insert into member_coaching_programs(member_no, program_no, remark, status, request_date, pay_date,
+                        start_date, end_date, star_rate, review, review_date, etc)
+  values(1, 1, '허리가 아파요', '진행완료', '2020-01-01', '2020-01-01', '2020-01-10', '2020-02-01',
+         5, '후기입니다', '2020-02-01', null);
+insert into member_coaching_programs(member_no, program_no, remark, status, request_date, pay_date,
+                        start_date, end_date, star_rate, review, review_date, etc)
+  values(2, 2, '허리가 아파요', '진행 완료', '2020-01-01', '2020-01-01', '2020-01-10', '2020-02-01',
+         5, '후기입니다', '2020-02-01', null);
+insert into member_coaching_programs(member_no, program_no, remark, status, request_date, pay_date,
+                        start_date, end_date, star_rate, review, review_date, etc)
+  values(3, 3, '허리가 아파요', '진행 완료', '2020-01-01', '2020-01-01', '2020-01-10', '2020-02-01',
+         5, '후기입니다', '2020-02-01', null);
+insert into member_coaching_programs(member_no, program_no, remark, status, request_date, pay_date,
+                        start_date, end_date, star_rate, review, review_date, etc)
+  values(4, 4, '허리가 아파요', '진행 완료', '2020-01-01', '2020-01-01', '2020-01-10', '2020-02-01',
+         5, '후기입니다', '2020-02-01', null);
+insert into member_coaching_programs(member_no, program_no, remark, status, request_date, pay_date,
+                        start_date, end_date, star_rate, review, review_date, etc)
+  values(5, 5, '허리가 아파요', '진행 완료', '2020-01-01', '2020-01-01', '2020-01-10', '2020-02-01',
+         5, '후기입니다', '2020-02-01', null);
+insert into member_coaching_programs(member_no, program_no, remark, status, request_date, pay_date,
+                        start_date, end_date, star_rate, review, review_date, etc)
+  values(6, 5, '허리가 아파요', '진행중', '2020-01-01', '2020-01-01', '2020-01-10', '2020-05-01',
+         null, null, '2020-02-01', null);
+insert into member_coaching_programs(member_no, program_no, remark, status, request_date, pay_date,
+                        start_date, end_date, star_rate, review, review_date, etc)
+  values(7, 4, '허리가 아파요', '진행중', '2020-01-01', '2020-01-01', '2020-01-10', '2020-05-01',
+         null, null, '2020-02-01', null);
+insert into member_coaching_programs(member_no, program_no, remark, status, request_date, pay_date,
+                        start_date, end_date, star_rate, review, review_date, etc)
+  values(8, 3, '허리가 아파요', '요청대기중', '2020-01-01', null, '2020-04-10', '2020-05-01',
+         null, null, '2020-02-01', null);
+insert into member_coaching_programs(member_no, program_no, remark, status, request_date, pay_date,
+                        start_date, end_date, star_rate, review, review_date, etc)
+  values(9, 2, '허리가 아파요', '요청거절됨', '2020-01-01', null, '2020-04-10', '2020-05-01',
+         null, null, '2020-02-01', '허리가 아프신 분은 수강이 힘드세요.');
+insert into member_coaching_programs(member_no, program_no, remark, status, request_date, pay_date,
+                        start_date, end_date, star_rate, review, review_date, etc)
+  values(10, 1, '허리가 아파요', '결제대기중', '2020-01-01', null, '2020-04-10', '2020-05-01',
+         null, null, '2020-02-01', null);
+  
 
 -- 회원코칭프로그램일정
 insert into member_program_calendar(member_program_no, plan)
@@ -239,6 +278,30 @@ insert into member_program_calendar(member_program_no, plan)
     insert into coaching_program_boards(coach_no, program_no,content)
   values(10,10,'내용');
   
+  
+  -- food_boards 예제 데이터
+insert into food_boards(photo, content)
+  values('cat1.jpg', '귀여운 고양이1와 식사했어요!');
+insert into food_boards(photo, content)
+  values('cat2.jpg', '귀여운 고양이2와 식사했어요!');
+insert into food_boards(photo, content)
+  values('cat3.jpg', '귀여운 고양이3와 식사했어요!');
+insert into food_boards(photo, content)
+  values('cat4.jpg', '귀여운 고양이4와 식사했어요!');
+insert into food_boards(photo, content)
+  values('cat5.jpg', '귀여운 고양이5와 식사했어요!');
+insert into food_boards(member_program_no, photo, content)
+  values(1, 'cat6.jpg', '귀여운 고양이6와 식사했어요!');
+insert into food_boards(member_program_no, photo, content)
+  values(2, 'cat7.jpg', '귀여운 고양이7와 식사했어요!');
+insert into food_boards(member_program_no, photo, content)
+  values(3, 'cat8.jpg', '귀여운 고양이8와 식사했어요!');
+insert into food_boards(member_program_no, photo, content)
+  values(4, 'cat9.jpg', '귀여운 고양이9와 식사했어요!');
+insert into food_boards(member_program_no, photo, content)
+  values(5, 'cat10.jpg', '귀여운 고양이10와 식사했어요!');
+  
+  
   --음식게시판댓글
    insert into food_board_comments(food_board_no, coach_no,content)
   values(1,1,'내용');
@@ -261,93 +324,6 @@ insert into member_program_calendar(member_program_no, plan)
      insert into food_board_comments(food_board_no, coach_no,content)
   values(10,10,'내용');
   
-  
--- food_boards 예제 데이터
-insert into food_boards(photo, content)
-  values('cat1.jpg', '귀여운 고양이1와 식사했어요!');
-insert into food_boards(photo, content)
-  values('cat2.jpg', '귀여운 고양이2와 식사했어요!');
-insert into food_boards(photo, content)
-  values('cat3.jpg', '귀여운 고양이3와 식사했어요!');
-insert into food_boards(photo, content)
-  values('cat4.jpg', '귀여운 고양이4와 식사했어요!');
-insert into food_boards(photo, content)
-  values('cat5.jpg', '귀여운 고양이5와 식사했어요!');
-insert into food_boards(member_program_no, photo, content)
-  values(1, 'cat6.jpg', '귀여운 고양이6와 식사했어요!');
-insert into food_boards(member_program_no, photo, content)
-  values(2, 'cat7.jpg', '귀여운 고양이7와 식사했어요!');
-insert into food_boards(member_program_no, photo, content)
-  values(3, 'cat8.jpg', '귀여운 고양이8와 식사했어요!');
-insert into food_boards(member_program_no, photo, content)
-  values(4, 'cat9.jpg', '귀여운 고양이9와 식사했어요!');
-insert into food_boards(member_program_no, photo, content)
-  values(5, 'cat10.jpg', '귀여운 고양이10와 식사했어요!');
-
--- member_coaching_programs 예제 데이터
-insert into food_boards(member_no, program_no, remark, status, request_date, pay_date,
-                        start_date, end_date, star_rate, review, review_date, etc)
-  values(1, 1, '허리가 아파요', '진행완료', '2020-01-01', '2020-01-01', '2020-01-10', '2020-02-01',
-         5, '후기입니다', '2020-02-01', null);
-insert into food_boards(member_no, program_no, remark, status, request_date, pay_date,
-                        start_date, end_date, star_rate, review, review_date, etc)
-  values(2, 2, '허리가 아파요', '진행 완료', '2020-01-01', '2020-01-01', '2020-01-10', '2020-02-01',
-         5, '후기입니다', '2020-02-01', null);
-insert into food_boards(member_no, program_no, remark, status, request_date, pay_date,
-                        start_date, end_date, star_rate, review, review_date, etc)
-  values(3, 3, '허리가 아파요', '진행 완료', '2020-01-01', '2020-01-01', '2020-01-10', '2020-02-01',
-         5, '후기입니다', '2020-02-01', null);
-insert into food_boards(member_no, program_no, remark, status, request_date, pay_date,
-                        start_date, end_date, star_rate, review, review_date, etc)
-  values(4, 4, '허리가 아파요', '진행 완료', '2020-01-01', '2020-01-01', '2020-01-10', '2020-02-01',
-         5, '후기입니다', '2020-02-01', null);
-insert into food_boards(member_no, program_no, remark, status, request_date, pay_date,
-                        start_date, end_date, star_rate, review, review_date, etc)
-  values(5, 5, '허리가 아파요', '진행 완료', '2020-01-01', '2020-01-01', '2020-01-10', '2020-02-01',
-         5, '후기입니다', '2020-02-01', null);
-insert into food_boards(member_no, program_no, remark, status, request_date, pay_date,
-                        start_date, end_date, star_rate, review, review_date, etc)
-  values(6, 5, '허리가 아파요', '진행중', '2020-01-01', '2020-01-01', '2020-01-10', '2020-05-01',
-         null, null, '2020-02-01', null);
-insert into food_boards(member_no, program_no, remark, status, request_date, pay_date,
-                        start_date, end_date, star_rate, review, review_date, etc)
-  values(7, 4, '허리가 아파요', '진행중', '2020-01-01', '2020-01-01', '2020-01-10', '2020-05-01',
-         null, null, '2020-02-01', null);
-insert into food_boards(member_no, program_no, remark, status, request_date, pay_date,
-                        start_date, end_date, star_rate, review, review_date, etc)
-  values(8, 3, '허리가 아파요', '요청대기중', '2020-01-01', null, '2020-04-10', '2020-05-01',
-         null, null, '2020-02-01', null);
-insert into food_boards(member_no, program_no, remark, status, request_date, pay_date,
-                        start_date, end_date, star_rate, review, review_date, etc)
-  values(9, 2, '허리가 아파요', '요청거절됨', '2020-01-01', null, '2020-04-10', '2020-05-01',
-         null, null, '2020-02-01', '허리가 아프신 분은 수강이 힘드세요.');
-insert into food_boards(member_no, program_no, remark, status, request_date, pay_date,
-                        start_date, end_date, star_rate, review, review_date, etc)
-  values(10, 1, '허리가 아파요', '결제대기중', '2020-01-01', null, '2020-04-10', '2020-05-01',
-         null, null, '2020-02-01', null);
-
-         
--- 운동내역 예제 데이터
-insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
-  values(1, 1, '2020-01-01', '60', '800');
-insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
-  values(1, 2, '2020-01-02', '40', '500');
-insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
-  values(1, 3, '2020-01-03', '100', '1000');
-insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
-  values(2, 1, '2020-02-01', '120', '1200');
-insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
-  values(2, 2, '2020-02-01', '30', '400');
-insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
-  values(3, 1, '2020-03-03', '10', '50');
-insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
-  values(4, 2, '2020-03-05', '70', '900');
-insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
-  values(5, 3, '2020-04-03', '60', '400');
-insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
-  values(6, 4, '2020-04-03', '80', '800');
-insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
-  values(7, 3, '2020-05-01', '40', '500');
   
   
 -- 운동 예제 데이터
@@ -373,6 +349,29 @@ insert into workout(workout_no, name, unit)
   values(10, '배드민턴', '분');
   
   
+  -- 운동내역 예제 데이터
+insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
+  values(1, 1, '2020-01-01', '60', '800');
+insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
+  values(1, 2, '2020-01-02', '40', '500');
+insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
+  values(1, 3, '2020-01-03', '100', '1000');
+insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
+  values(2, 1, '2020-02-01', '120', '1200');
+insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
+  values(2, 2, '2020-02-01', '30', '400');
+insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
+  values(3, 1, '2020-03-03', '10', '50');
+insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
+  values(4, 2, '2020-03-05', '70', '900');
+insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
+  values(5, 3, '2020-04-03', '60', '400');
+insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
+  values(6, 4, '2020-04-03', '80', '800');
+insert into workout_list(member_no, workout_no, workout_date, workout_amount, kcal)
+  values(7, 3, '2020-05-01', '40', '500');
+  
+  
 -- 캘린터 첨부파일
 insert into calendar_files(file_no, calendar_no, path, file_type)
   values(1, 1, 'user/user/com/bitcamp', 'a.gif');
@@ -381,11 +380,11 @@ insert into calendar_files(file_no, calendar_no, path, file_type)
 insert into calendar_files(file_no, calendar_no, path, file_type)
   values(3, 2, 'user/user/com/bitcamp2', 'b1.mp4');
 insert into calendar_files(file_no, calendar_no, path, file_type)
-  values(3, 3, 'user/user/com/bitcamp2', 'b2.png');
+  values(4, 3, 'user/user/com/bitcamp2', 'b2.png');
 insert into calendar_files(file_no, calendar_no, path, file_type)
-  values(4, 4, 'user/user/com/bitcamp3', 'c1.png');
+  values(5, 4, 'user/user/com/bitcamp3', 'c1.png');
 insert into calendar_files(file_no, calendar_no, path, file_type)
-  values(4, 4, 'users/users/com/bitcamp3', 'd1.gif');
+  values(6, 4, 'users/users/com/bitcamp3', 'd1.gif');
 insert into calendar_files(file_no, calendar_no, path, file_type)
   values(7, 5, 'users/users/com/bitcamp4', 'a3.mp4');
 insert into calendar_files(file_no, calendar_no, path, file_type)
@@ -394,7 +393,72 @@ insert into calendar_files(file_no, calendar_no, path, file_type)
   values(9, 2, 'users/users/com/bitcamp6', 'e2.gif');
 insert into calendar_files(file_no, calendar_no, path, file_type)
   values(10, 4, 'users/users/com/bitcamp9', 'e3.mp4');
+  
+  -- 운동태그
+insert into workout_tags(tag_no, name)
+  values (1,'하체튼튼');
+insert into workout_tags(tag_no, name)
+  values (2,'상체튼튼');
+insert into workout_tags(tag_no, name)
+  values (3,'근육위주');
+insert into workout_tags(tag_no, name)
+  values (4,'대회위주');
+insert into workout_tags(tag_no, name)
+  values (5,'체력위주');
+insert into workout_tags(tag_no, name)
+  values (6,'체중감략');
+insert into workout_tags(tag_no, name)
+  values (7,'제활위주');
+insert into workout_tags(tag_no, name)
+  values (8,'생활개선');
  
+  
+-- 코칭프로그램운동태그
+insert into coaching_program_tags(tag_no, program_no)
+  values (1,1);
+insert into coaching_program_tags(tag_no, program_no)
+  values (1,2);
+insert into coaching_program_tags(tag_no, program_no)
+  values (2,3);
+insert into coaching_program_tags(tag_no, program_no)
+  values (3,3);
+insert into coaching_program_tags(tag_no, program_no)
+  values (3,4);
+insert into coaching_program_tags(tag_no, program_no)
+  values (4,2);
+insert into coaching_program_tags(tag_no, program_no)
+  values (5,6);
+insert into coaching_program_tags(tag_no, program_no)
+  values (6,5);
+insert into coaching_program_tags(tag_no, program_no)
+  values (6,3);
+insert into coaching_program_tags(tag_no, program_no)
+  values (8,3);
+  
+  
+--코칭프로그램방식
+insert into program_coaching_systems(coaching_system_no, program_no)
+  values (1, 1);
+insert into program_coaching_systems(coaching_system_no, program_no)
+  values (2, 2);
+insert into program_coaching_systems(coaching_system_no, program_no)
+  values (3, 3);
+insert into program_coaching_systems(coaching_system_no, program_no)
+  values (4, 4);
+insert into program_coaching_systems(coaching_system_no, program_no)
+  values (5, 5);
+insert into program_coaching_systems(coaching_system_no, program_no)
+  values (6, 6);
+insert into program_coaching_systems(coaching_system_no, program_no)
+  values (7, 7);
+insert into program_coaching_systems(coaching_system_no, program_no)
+  values (8, 8);
+insert into program_coaching_systems(coaching_system_no, program_no)
+  values (9, 9);
+insert into program_coaching_systems(coaching_system_no, program_no)
+  values (10, 10);
+  
+
   
 
 
