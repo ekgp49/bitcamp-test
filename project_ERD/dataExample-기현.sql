2b4c87756572e87ece838375b1312410a4d0df72
rogihyun <srlgus2002v@naver.com>
2020-04-06 17:46:29 +0900

    수정

--- project_ERD/dataExample-기현.sql
+++ project_ERD/dataExample-기현.sql
@@ -1,3 +1,4 @@
+
 -- 운동태그 workout_tags
 
 insert into  workout_tags(tag_no, name )
@@ -28,35 +29,35 @@ value (3, '온라인+오프라인');
 
 -- 코칭프로그램운동태그 coaching_program_tags
 
-insert into  coaching_program_tags(tag_no,  program_no,content)
-value (1, 1, '운동태그1');
+insert into  coaching_program_tags(tag_no,  program_no)
+value (1, 1,);
 
-insert into  coaching_program_tags(tag_no,  program_no,content)
-value (1, 1, '운동태그2');
+insert into  coaching_program_tags(tag_no,  program_no)
+value (2, 2,);
 
-insert into  coaching_program_tags(tag_no,  program_no,content)
-value (2, 2, '운동태그3');
+insert into  coaching_program_tags(tag_no,  program_no)
+value (3, 3,);
 
+insert into  coaching_program_tags(tag_no,  program_no)
+value (4, 4,);
 
-insert into  coaching_program_tags(tag_no,  program_no,content)
-value (3, 3, '운동태그4');
-
-insert into  coaching_program_tags(tag_no,  program_no,content)
-value (4, 4, '운동태그5');
-
-
+insert into  coaching_program_tags(tag_no,  program_no)
+value (5, 5,);
 
 -- 코칭프로그램방식 program_coaching_systems 
 
 insert into   program_coaching_systems  (coaching_system_no, program_no,content)
-values(1, 1,'운동방식1')
+values(1, 1)
 
 insert into   program_coaching_systems  (coaching_system_no, program_no,content)
-values(1, 1,'운동방식2')
+values(2, 2)
 
 insert into   program_coaching_systems  (coaching_system_no, program_no,content)
-values(2, 2,'운동방식3')
+values(3, 3)
 
 insert into   program_coaching_systems  (coaching_system_no, program_no,content)
-values(3, 3,'운동방식4')
+values(4, 4)
+
+insert into   program_coaching_systems  (coaching_system_no, program_no,content)
+values(5, 5)
 
